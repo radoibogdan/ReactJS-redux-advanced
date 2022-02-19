fbdf6cab05193f69ec65f26130535c1dc1300953
YBogdan RADOI <radoi.office@gmail.com>
2022-02-19 17:55:41 +0100

    Http request with useEffect + Firebase

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
@@ -5,7 +6,15 @@ import Products from './components/Shop/Products';
 
 function App() {
   const showCart = useSelector(store => store.ui.cartIsVisible)
-
+  const cart = useSelector(store => store.cart);
+  
+  useEffect(() => {
+    fetch('https://react-movie-http-95492-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {
+      method : 'PUT',
+      body: JSON.stringify(cart)
+    })
+  }, [cart])
+  // https://react-movie-http-95492-default-rtdb.europe-west1.firebasedatabase.app/
   return (
     <Layout>
       {showCart && <Cart />}
